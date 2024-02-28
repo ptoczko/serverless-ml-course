99a23f4ca8fdb7bed9532b3fb552dc05af00e49d
Patryk Toczko <ptoczko07@gmail.com>
2024-02-27 22:44:19 -0500

    Remove extra files

--- src/01-module/scripts/run-feature-and-prediction-pipelines.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cd src/01-module
-
-jupyter nbconvert --to notebook --execute iris-feature-pipeline.ipynb
-jupyter nbconvert --to notebook --execute iris-batch-inference-pipeline.ipynb
-
